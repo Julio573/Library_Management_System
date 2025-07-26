eaef60d11d6cbd9557bebc4b855160a942881b97
julio573 <julio8497@hotmail.com>
2025-07-26 10:37:58 -0300

    feat: Add Author Entity

---
+++ src/model/Author.java
@@ -0,0 +1,63 @@
+package model;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class Author {
+
+    private String name;
+    private Date birthDate;
+    private String nationality;
+    private String biography;
+
+    public Author(String name, Date birthDate, String nationality, String biography) {
+        this.name = name;
+        this.birthDate = birthDate;
+        this.nationality = nationality;
+        this.biography = biography;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(name, author.name) && Objects.equals(biography, author.biography);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, biography);
+    }
+}
